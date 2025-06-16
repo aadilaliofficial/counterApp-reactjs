23c2f56949d40575cd308c852f8a407fb73d1396
Aadil Ali <61555486+aadilaliofficial@users.noreply.github.com>
2025-06-16 17:34:19 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Counter from './component/Counter'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function App() {
+  return (
+    <div> 
+      <Counter/>
+    </div>
+  )
+}
